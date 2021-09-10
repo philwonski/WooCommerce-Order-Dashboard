847a37929950465f01c487543e5b6d29e97f7efa
Philip Wonski <philip318@gmail.com>
2021-09-10 07:21:33 -0400

    fixed dater.sh format

--- server/dater.sh
+++ server/dater.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # create date file
-cat > /root/server/tiddlers/DayCheck.tid << EOF
+cat > /root/thdash/tiddlers/DayCheck.tid << EOF
 title: DayCheck
 tag: robot
-today: $(date +%B\%e,\ %Y)
+today: $(date +%B\ %d,\ %Y)
 EOF
